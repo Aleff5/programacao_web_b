7c9d0be26d17fe2f99b814c73620d4149d0ab531
aleff campos <aleff.mc@sempreceub.com>
2024-08-20 08:58:03 -0300

    javascript e ft de fundo

---
+++ script.js
@@ -0,0 +1,51 @@
+function submeter(){
+    let nome = document.getElementById("nome").ariaValueMax;
+    let cpf = document.getElementById("cpf").ariaValueMax;
+    let  idade = document.getElementById("idade").ariaValueMax;
+    console.log(validaCpf(cpf))
+}
+
+function validarCPF(cpf) {
+    
+    cpf = cpf.replace(/[^\d]+/g, '');
+
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+        return false;
+    }
+    // 021 818 446 15
+
+    // let soma = 0;
+    // for( let i = 0; i <= 9; i++){
+    //     soma += parseInt(cpf.charAt(i)) * (10 - i)
+    // }
+    // let resto = (soma % 11);
+    
+
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    console.log(soma)
+    let resto = soma % 11;
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+    if (resto !== parseInt(cpf.charAt(9))) {
+        return false;
+    }
+
+    
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+    if (resto !== parseInt(cpf.charAt(10))) {
+        return false;
+    }
+
+    return true;
+}
